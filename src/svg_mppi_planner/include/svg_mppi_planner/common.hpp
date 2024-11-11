45d206ad92fc09c4f1a084ecf795e654866e220d
jinooLi <jinu1423@naver.com>
2024-11-11 14:40:29 +0000

    Add and edit comments

--- src/svg_mppi_planner/include/svg_mppi_planner/common.hpp
+++ src/svg_mppi_planner/include/svg_mppi_planner/common.hpp
@@ -5,18 +5,26 @@
 
 namespace svg_mppi {
 
+/**
+ * @brief ko)상태공간내 원소의 순서(인덱스)와 상태공간의 차원 정의 
+ * @brief en)Definition of the order (index) and dimension of the elements in the state space
+ */
 namespace STATE_SPACE {
-    static constexpr int x = 0;
-    static constexpr int y = 1;
-    static constexpr int yaw = 2;
-    static constexpr int velocity = 3;
-    static constexpr int steering = 4;
-    static constexpr int dim = 5;
+    static constexpr int x = 0; // x-coordinate index
+    static constexpr int y = 1; // y-coordinate index
+    static constexpr int yaw = 2; // yaw angle index
+    static constexpr int velocity = 3; // velocity index
+    static constexpr int steering = 4; // steering angle index
+    static constexpr int dim = 5; // dimension of state space
 } // STATE_SPACE
 
+/**
+ * @brief ko)제어공간의 순서(인덱스)와 차원 정의 
+ * @brief en)Definition of the order (index) and dimension of the control space
+ */
 namespace CONTROL_SPACE {
-    static constexpr int steering = 0;
-    static constexpr int dim = 1;
+    static constexpr int steering = 0; // steering angle index
+    static constexpr int dim = 1; // dimension of control space
 } // CONTROL_SPACE
 
 struct Parameters {
@@ -46,10 +54,10 @@ namespace planning {
     using ControlSequence = Eigen::MatrixXd;
 
     // 각 원소가 Matrix인 Vector.
-    // 이때, Matrix는 각각의 column vector가 하나의 case에 포함되는 state들을 의미한다. 
+    // 이때, 하나의 Matrix는 한 trajectory 내 각각의 state vector가 column vector인 Matrix이다.
     using StateSequenceBatch = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>;
     // 각 원소가 Matrix인 Vector.
-    // 이때, Matrix는 각각의 column vector가 하나의 case에 포함되는 control들을 의미한다.
+    // 이때, 하나의 Matrix는 한 trajectory 내 각각의 control vector가 column vector인 Matrix이다.
     using ControlSequenceBatch = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>;
 
     using ControlCovarianceSequence = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>;
